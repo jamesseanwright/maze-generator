61233eb063b0a2d786239544c3823e91bdb258b6
James Wright <james@jamesswright.co.uk>
2017-08-27 14:33:22 +0100

    Use babel-eslint to support dangling commas

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+    "parser": "babel-eslint",
     "env": {
         "es6": true,
         "commonjs": true,
@@ -11,26 +12,11 @@ module.exports = {
     "extends": "eslint:recommended",
 
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-
-        "quotes": [
-            "error",
-            "single"
-        ],
-
-        "semi": [
-            "error",
-            "always"
-        ],
-
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"],
         "no-var": "error",
+        "comma-dangle": ["error", "always-multiline"],
     }
 };
